5798448e33711bbe70497da9428348d3f7206d00
Dave Tickle <dave.tickle@socialfinance.org.uk>
2022-10-04 14:26:30 +0100

    feat: invoking call to shell, handle IO

--- applet.js
+++ applet.js
@@ -1,5 +1,15 @@
 const Applet = imports.ui.applet;
 const Util = imports.misc.util;
+const PATH = "~/.local/share/cinnamon/applets/cats-lock@cinnamon.org";
+const STATES = {
+  unlocked: {
+    desc: "Keyboard UNLOCKED. Click here to lock",
+    icon: "cat",
+  },
+  locked: {
+    desc: "Keyboard LOCKED. Click here to unlock",
+  },
+};
 
 function MyApplet(orientation, panel_height, instance_id) {
   this._init(orientation, panel_height, instance_id);
@@ -16,12 +26,17 @@ MyApplet.prototype = {
       instance_id
     );
 
-    this.set_applet_icon_name("cat");
-    this.set_applet_tooltip(_("Click here to lock/unlock keyboard"));
+    this.set_applet_icon_name(STATES.unlocked.icon);
+    this.set_applet_tooltip(_(STATES.unlocked.desc));
   },
 
   on_applet_clicked: function () {
-    Util.spawnCommandLine("code");
+    Util.spawnCommandLineAsyncIO(
+      "sh " + PATH + "/cmd.sh",
+      function (stdOut, stdErr) {
+        global.log(stdOut, stdErr);
+      }
+    );
   },
 };
 
